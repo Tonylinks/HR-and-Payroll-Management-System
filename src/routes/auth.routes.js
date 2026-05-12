d543eaa138072067f0f198fe5b991b7d9085d8a9
Davsaltoby <davsaltobi@gmail.com>
2026-05-12 04:02:36 +0100

    fixed typos in signInController, authentication file, and Installed bcrypt package

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import Router from 'express'
 import signUpController from '../controllers/auth/signUpController.js'
-import sigInController from '../controllers/auth/signInController.js'
+import signInController from '../controllers/auth/signInController.js'
 import signUpValidation from '../middlewares/auth/signUpValidation.js'
 import signInValidation from '../middlewares/auth/signInValidation.js'
 
@@ -8,7 +8,7 @@ const authRouter = Router()
 
 authRouter.post('/sign-up', signUpValidation, signUpController)
 
-authRouter.post('/sign-in', signInValidation, sigInController)
+authRouter.post('/sign-in', signInValidation, signInController)
 
 authRouter.post('/sign-out', (req, res) => {
   // Handle user logout
