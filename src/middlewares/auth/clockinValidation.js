0fda15922ba34e4d05d09d780d7662a6f7b77cc7
owojaiye <samuelowojaiye404@gmail.com>
2026-05-14 15:06:34 +0100

    Attendance Model, Controller and Validation Created and Tested

---
+++ src/middlewares/auth/clockinValidation.js
@@ -0,0 +1,36 @@
+import Attendance from '../../models/attendance.js'
+
+const clockinValidation = async (req, res, next) => {
+  const { employeeId } = req.body
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
+
+  const endOfDay = new Date()
+  endOfDay.setHours(23, 59, 59, 999)
+  try {
+    const existingAttendance = await Attendance.findOne({
+      employeeId,
+      date: {
+        $gte: today,
+        $lte: endOfDay
+      },
+      clockOut: null
+    })
+
+    if (existingAttendance) {
+      return res.status(400).json({
+        success: false,
+        message: 'Employee already clocked in and has not clocked out'
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
+
+  next()
+}
+
+export default clockinValidation
